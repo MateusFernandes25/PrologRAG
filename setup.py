715b217fe2391068eb86a1e3852c7f8cfedd3d91
Mateus Fernandes <64925284+MateusFernandes25@users.noreply.github.com>
2025-02-08 09:20:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author="Mateus Fernandes",
     description="PrologRAG is an innovative framework that leverages the concept of Retrieval-Augmented Generation (RAG) to organize and answer queries about documents in Prolog format. This project extracts knowledge from documents, transforms it into structured Prolog code, and provides accurate and contextual answers to questions.",
     packages=find_packages(),
-    install_requires=["pyswip", "langchain_core","langchain_openai","langchain"],
+    install_requires=["pyswip", "langchain_core","langchain_openai","langchain","langchain-community","pypdf"],
     python_requires=">=3.8",
 )
